46975c75137d2e98b97885e190a10a5e19be3f65
satzazamaga <vb507019@gmail.com>
2025-05-30 10:37:53 +0500

    Update UlbaInfo.java

--- UlbaInfo.java
+++ UlbaInfo.java
@@ -2,8 +2,9 @@ public class UlbaInfo {
     public static void main(String[] args) {
         System.out.println("=== Ульбинский Металлургический Завод ===");
         System.out.println("Год основания: 1949");
-        System.out.println("Город: Усть-Каменогорск, Казахстан");
+        System.out.println("Расположение: Усть-Каменогорск, Казахстан");
         System.out.println("Продукция: Уран, Тантал, Бериллий, Ниобий");
-        System.out.println("УМЗ — надёжный поставщик редких металлов для мира.");
+        System.out.println("Персонал: более 5000 сотрудников");
+        System.out.println("Экспорт: в более чем 60 стран мира");
     }
 }
